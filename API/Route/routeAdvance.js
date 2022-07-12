d1569dfe0ed8c09ea8310697b198436f865a4b9a
pranav8121 <pranav8121patil@gmail.com>
2022-07-12 22:52:20 +0530

    Advance Route Added

--- API/Route/routeAdvance.js
+++ API/Route/routeAdvance.js
@@ -2,4 +2,7 @@ const express = require('express');
 var router = express.Router();
 const advanceController = require('../Controller/advanceController');
 
+router.post('/postAdvance', advanceController.postAdvance);
+router.post('/getAdvance', advanceController.getAdvance);
+
 module.exports = router;
